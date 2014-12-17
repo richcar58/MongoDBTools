a23a623c8a4dcce7efc7a24c98c51926b455e402
rich <richard.cardone@10gen.com>
2014-12-17 09:30:32 -0600

    Initial commit.

---
+++ RunYcsb/runycsb/SeriesEnv.py
@@ -0,0 +1,181 @@
+'''
+Created on Dec 12, 2014
+
+@author: rich
+'''
+# bin/mongod --dbpath /home/rich/pkgs/mongodb-latest/mongodb-linux-x86_64-2.8.0-rc1/data/db 
+# --logpath /mnt/Disk1-2TB/logs/mongo-thread/mongod-15million-12thread.log --storageEngine wiredTiger
+
+# time bin/ycsb load mongodb -p recordcount=15000000 -p operationcount=30000000 -p threadcount=1 -p hosts=localhost -P workloads/workloada
+
+from fabric.api import env
+import json, os, logging
+
+# Constants.
+SERIES_PROPERTY_FILE = 'seriesConfig.json'
+
+class SeriesEnv():
+    '''
+    classdocs
+    '''
+    
+    # --------------------------------------------------------
+    # Fields
+    # --------------------------------------------------------
+    # The mapping of configuration parameters 
+    # read from the configuration json file.
+    seriesConfig = {}  
+    
+    # Paths calculated from input file parameters.
+    logpath = None
+    dbpath = None
+    
+    # --------------------------------------------------------
+    # Constructor
+    # --------------------------------------------------------
+    def __init__(self):
+        '''
+        Constructor
+        '''
+        
+        # Set logging level.
+        logging.root.setLevel(logging.INFO)
+        
+        # Properties expected in the config file are:
+        #
+        #  hosts                mandatory    array of string
+        #  dbpath_root          mandatory    string, path to database directory
+        #  logpath_root         mandatory    string, path to root directory for all logs
+        #  series_name          mandatory    string, subdirectory name for this run's mongo and ycsb logs 
+        #
+        #  ycsb_bin_path        mandatory    string, path to ycsb bin directory
+        #  ycsb_operationcount  optional     integer (ignores negative numbers)
+        #  ycsb_recordcount     mandatory    integer
+        #  ycsb_threadcount     mandatory    integer
+        #  ycsb_workloads       mandatory    array of string
+        #   
+        #  mongo_bin_path       mandatory    string, path to bin directory containing mongod 
+        #  mongo_parms          mandatory    string, all parms other than --dbpath and --logpath 
+        #                                            - specify storageEngine with its parms here
+        #   
+        # Read the configuration file.
+        with open(SERIES_PROPERTY_FILE, 'r') as fp:
+            self.seriesConfig = json.load(fp)
+            
+        # Validate the configuration. 
+        self._validateConfig(self.seriesConfig)
+        
+        # Process the configuration.
+        self._processConfig(self.seriesConfig)
+        
+        # Write configuration debug message   
+        logging.debug("----- seriesConfig:")
+        logging.debug(self.seriesConfig)
+            
+    # --------------------------------------------------------
+    # _validateConfig
+    # --------------------------------------------------------
+    def _validateConfig(self, config):
+            
+        # Check hosts array
+        if (not config.has_key('hosts')) or (not config['hosts']) or (not isinstance(config['hosts'], list)):
+            msg = "The hosts parameter is missing, empty or non-array in configuration file " \
+                  + SERIES_PROPERTY_FILE + "."
+            raise Exception(msg) 
+            
+        # Check for missing dbpath_root
+        if (not config.has_key('dbpath_root')) or (not config['dbpath_root']):
+            msg = "The dbpath_root parameter is missing or empty in configuration file " \
+                  + SERIES_PROPERTY_FILE + "."
+            raise Exception(msg) 
+            
+        # Check for missing logpath_root
+        if (not config.has_key('logpath_root')) or (not config['logpath_root']):
+            msg = "The logpath_root parameter is missing or empty in configuration file " \
+                  + SERIES_PROPERTY_FILE + "."
+            raise Exception(msg) 
+            
+        # Check for missing mongo_bin_path
+        if (not config.has_key('mongo_bin_path')) or (not config['mongo_bin_path']):
+            msg = "The mongo_bin_path parameter is missing or empty in configuration file " \
+                  + SERIES_PROPERTY_FILE + "."
+            raise Exception(msg) 
+        
+        # Check for missing mongo_parms
+        if (not config.has_key('mongo_parms')) or (not config['mongo_parms']) \
+            or (not isinstance(config['mongo_parms'], list)):
+            msg = "The mongo_storage_engine parameter is missing or empty in configuration file " \
+                  + SERIES_PROPERTY_FILE + "."
+            raise Exception(msg) 
+            
+        # Check for missing ycsb_bin_path
+        if (not config.has_key('ycsb_bin_path')) or (not config['ycsb_bin_path']):
+            msg = "The ycsb_bin_path parameter is missing or empty in configuration file " \
+                  + SERIES_PROPERTY_FILE + "."
+            raise Exception(msg) 
+        
+        # Check ycsb_workloads array
+        if (not config.has_key('ycsb_workloads')) or (not config['ycsb_workloads']) \
+                or (not isinstance(config['ycsb_workloads'], list)):
+            msg = "The ycsb_workloads parameter is missing or empty in configuration file " \
+                  + SERIES_PROPERTY_FILE + "."
+            raise Exception(msg) 
+            
+        # Check ycsb_recordcount array
+        if (not config.has_key('ycsb_recordcount')) or (not config['ycsb_recordcount']) \
+                or (not isinstance(config['ycsb_recordcount'], list)):
+            msg = "The ycsb_recordcount parameter is missing, empty or non-array in configuration file " \
+                  + SERIES_PROPERTY_FILE + "."
+            raise Exception(msg) 
+         
+        # Check ycsb_operationcount array
+        # If the key exists and is a non-empty array, it must have the same number of elements
+        # as the ycsb_recordcount array.  If it's not an array, it will be replaced by an
+        # array in the process method.
+        if config.has_key('ycsb_operationcount') and isinstance(config['ycsb_operationcount'], list) \
+                and config['ycsb_operationcount'] \
+                and (not len(config['ycsb_operationcount']) == len(config['ycsb_recordcount'])):
+            msg = "Arrays ycsb_recordcount and ycsb_operationcount are not the same length in configuration file " \
+                  + SERIES_PROPERTY_FILE + "."
+            raise Exception(msg) 
+         
+        # Check ycsb_threadcount
+        if (not config.has_key('ycsb_threadcount')) or (not config['ycsb_threadcount']):
+            msg = "The ycsb_threadcount parameter is missing or empty in configuration file " \
+                  + SERIES_PROPERTY_FILE + "."
+            raise Exception(msg) 
+            
+        # Check series_name
+        if (not config.has_key('series_name')) or (not config['series_name']):
+            msg = "The series_name parameter is missing or empty in configuration file " \
+                  + SERIES_PROPERTY_FILE + "."
+            raise Exception(msg) 
+            
+    # --------------------------------------------------------
+    # _processConfig
+    # --------------------------------------------------------
+    def _processConfig(self, config):
+            
+        # Assign fabric env hosts variable
+        env.hosts = config['hosts']
+            
+        # Construct mongo_logpath (assumes Linux)
+        self.logpath = os.path.join(config['logpath_root'], config['series_name'])
+           
+        # Construct mongo_dbpath (assumes Linux)
+        self.dbpath = config['dbpath_root']
+            
+        # Assign default operation count if none provided.  On average,
+        # we do a read and an update on each record.
+        if (not config.has_key('ycsb_operationcount')) or (not config['ycsb_operationcount']) \
+                or (not isinstance(config['ycsb_operationcount'], list)):
+            config['ycsb_operationcount'] = map(lambda x : 2 * x, config['ycsb_recordcount'][:]) 
+        
+# -------------------------------------------------------- 
+# Main
+# --------------------------------------------------------
+if __name__ == '__main__':
+    x = SeriesEnv()
+    logging.info("SeriesEnv settings:")
+    logging.info(vars(x))
+
---
+++ RunYcsb/runycsb/fabfile.py
@@ -0,0 +1,204 @@
+'''
+Created on Dec 12, 2014
+
+@author: rich
+'''
+
+# Imports
+from SeriesEnv import SeriesEnv
+import sys, logging, os
+from datetime import datetime
+from time import sleep
+from fabric.api import run, settings, env
+from fabric.main import main
+
+# Sample commands.
+# bin/mongod --dbpath /home/rich/pkgs/mongodb-latest/mongodb-linux-x86_64-2.8.0-rc1/data/db 
+# --logpath /mnt/Disk1-2TB/logs/mongo-thread/mongod-15million-12thread.log --storageEngine wiredTiger
+
+# time bin/ycsb load mongodb -p recordcount=15000000 -p operationcount=30000000 -p threadcount=1 -p hosts=localhost -P workloads/workloada
+
+# --------------------------------------------------------
+# Fields
+# --------------------------------------------------------
+# The mapping of configuration parameters 
+# read from the configuration json file.
+seriesEnv = SeriesEnv()  
+
+# -------------------------------------------------------- 
+# run_all
+# --------------------------------------------------------
+def run_all():
+    """
+    Highest level test.
+    """ 
+    print(sys.argv)
+    logging.root.debug(vars(seriesEnv)) 
+    print(vars(seriesEnv))
+    run_mongo()
+    
+# -------------------------------------------------------- 
+# run_mongo
+# --------------------------------------------------------
+def run_mongo():
+    """
+    Run the mongo test suite
+    """
+    starttime = datetime.now()
+    print('>> Starting run_mongo [' + str(starttime) + ']') 
+    _mongo_stop()
+    _mongo_setup()
+    _mongo_clean()  
+    
+    # We know the recordCount and operationCount arrays are the
+    # same non-zero length, so the inner loop is sound.
+    for workload in seriesEnv.seriesConfig['ycsb_workloads']:
+        for j in range(len(seriesEnv.seriesConfig['mongo_parms'])):
+            for i in range(len(seriesEnv.seriesConfig['ycsb_recordcount'])):
+                # Get the corresponding counts from the two arrays.
+                recordCount = seriesEnv.seriesConfig['ycsb_recordcount'][i]
+                operationCount = seriesEnv.seriesConfig['ycsb_operationcount'][i]
+                
+                # Determine the storage abbreviation for file naming.
+                mongoParms = seriesEnv.seriesConfig['mongo_parms'][j]
+                storageAbbrev = _getStorageAbbreviation(mongoParms, j)
+                
+                # Start mongo, load and run ycsb, and clean up.
+                _mongo_start(recordCount, mongoParms, workload, storageAbbrev)
+                _ycsb('load', 'mongodb', recordCount, operationCount, workload, storageAbbrev)
+                _ycsb('run', 'mongodb', recordCount, operationCount, workload, storageAbbrev)
+                _mongo_stop()
+                _mongo_clean()
+
+    endtime = datetime.now()
+    print('>>>> Completing run_mongo [' + str(endtime) + ', duration = ' + str(endtime-starttime) + ']') 
+    
+# -------------------------------------------------------- 
+# mongo_clean
+# --------------------------------------------------------
+def _mongo_clean():
+    """
+    Remove all existing mongodb databases.
+    """
+    starttime = datetime.now()
+    print('>>>> Starting mongo_clean [' + str(starttime) + ']') 
+    with settings(warn_only=True):
+        run("rm -rf %s/*" % seriesEnv.dbpath)
+
+# -------------------------------------------------------- 
+# mongo_setup
+# --------------------------------------------------------
+def _mongo_setup():
+    """
+    Ensure that required directories exist.
+    """
+    starttime = datetime.now()
+    print('>>>> Starting mongo_setup [' + str(starttime) + ']') 
+    
+    # Construct mongo_logpath (assumes Linux)
+    with settings(warn_only=True):
+        if run("test -d %s" % seriesEnv.logpath).failed:
+            run("mkdir -p %s" % seriesEnv.logpath)
+           
+    # Construct mongo_dbpath (assumes Linux)
+    with settings(warn_only=True):
+        if run("test -d %s" % seriesEnv.dbpath).failed:
+            run("mkdir -p %s" % seriesEnv.dbpath)
+  
+# -------------------------------------------------------- 
+# mongo_start
+# --------------------------------------------------------
+def _mongo_start(recordCount, mongoParms, workload, storageAbbrev):
+    """
+    Start mongod in the background.
+    """
+    starttime = datetime.now()
+    print('>>>> Starting run_mongo [' + str(starttime) + ']') 
+    
+    # Start command string.
+    mongoCmd = os.path.join(seriesEnv.seriesConfig['mongo_bin_path'], 'mongod')
+    mongoCmd += " --dbpath " + seriesEnv.dbpath 
+    
+    # Add log file.
+    # Determine storage engine abbreviation for log naming purposes.
+    mongoCmd += " --logpath " + os.path.join(seriesEnv.logpath, 
+                _make_log_filename('mongod', storageAbbrev, recordCount,
+                     seriesEnv.seriesConfig['ycsb_threadcount'], workload))
+    
+    # Add all other parms.
+    mongoCmd += " " + mongoParms
+    mongoCmd += " --fork" 
+    run(mongoCmd)
+    sleep(8)
+
+# -------------------------------------------------------- 
+# mongo_stop
+# --------------------------------------------------------
+def _mongo_stop():
+    """Stop all mongod instances"""
+    starttime = datetime.now()
+    print('>>>> Starting mongo_stop [' + str(starttime) + ']') 
+    with settings(warn_only=True):
+        run('killall mongod')
+        sleep(5)
+
+# -------------------------------------------------------- 
+# _ycsb
+# --------------------------------------------------------
+def _ycsb(action, product, recordCount, operationCount, workload, storageAbbrev):
+    """Execute ycsb load or run actions."""
+    starttime = datetime.now()
+    print('>>>> Starting _ycsb [' + str(starttime) + ']') 
+    
+    # Construct the logfile name.
+    logfile = os.path.join(seriesEnv.logpath, 
+                    _make_log_filename('ycsb', storageAbbrev, recordCount, seriesEnv.seriesConfig['ycsb_threadcount'], workload))
+    
+    # Start command string.
+    ycsbCmd = os.path.join(seriesEnv.seriesConfig['ycsb_bin_path'], 'ycsb')
+    ycsbCmd += " " + action + " " + product 
+    ycsbCmd += " -p recordcount=" + str(recordCount)
+    ycsbCmd += " -p operationcount=" + str(operationCount) 
+    ycsbCmd += " -p threadcount=" + str(seriesEnv.seriesConfig['ycsb_threadcount']) 
+    ycsbCmd += " -p env.hosts=" + env.host
+    ycsbCmd += " -P " + os.path.normpath(os.path.join(seriesEnv.seriesConfig['ycsb_bin_path'], 
+                           "../workloads/"+workload)) 
+    ycsbCmd += " >> " + logfile + " 2>&1"
+    run(ycsbCmd)
+    
+    endtime = datetime.now()
+    print('>>>> Completing _ycsb [' + str(endtime) + ', duration = ' + str(endtime-starttime) + ']') 
+
+# -------------------------------------------------------- 
+# _make_log_filename
+# --------------------------------------------------------
+def _make_log_filename(program, storageAbbrev, records, threads, workload):
+    """Construct a log file name based on this run's parameters."""
+    fn = program + "-" + storageAbbrev
+    fn += "-" + workload
+    fn += "-" + str(records) + "recs"
+    fn += "-" + str(threads) + 'thrds.log'
+    return fn
+
+# -------------------------------------------------------- 
+# _getStorageAbbreviation
+# --------------------------------------------------------
+def _getStorageAbbreviation(mongoParms, index):
+    """Get the storage engine abbreviation for log naming."""
+    
+    # The elif statement helps futureproof for when wt becomes the default.
+    abbrev = "mm"
+    if mongoParms.find("wiredTiger") >= 0:
+        abbrev = "wt"
+    elif mongoParms.find("mmapv1") >= 0:
+        abbrev = "mm"
+    return abbrev + str(index)
+
+# -------------------------------------------------------- 
+# Main
+# --------------------------------------------------------
+# Allow execution from within Eclipse   
+if __name__ == '__main__':
+    sys.argv = ['fab', '-f', __file__, 'run_all']
+    main()    
+    
